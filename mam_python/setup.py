87f858f4237d54123333b6caedc1c7a74adb3a5d
NicolasPh7 <nicolas.philippe@etu.univ-smb.fr>
2025-11-12 19:48:41 +0100

    Complete ROS2 Humble integration - node running with all modules

--- mam_python/setup.py
+++ mam_python/setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
+from glob import glob
+import os
 
 setup(
     name='mam_python',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', 'launch']),
+    package_data={'mam_python': []},
+    data_files=[
+        (os.path.join('share', 'mam_python', 'launch'), glob(os.path.join('launch', '*.py'))),
+    ],
     install_requires=['setuptools'],
-    author='Nest Masters',
-    description='MAM Robot - Squirrel Cup Eurobot 2026',
-    license='MIT',
     entry_points={
         'console_scripts': [
-            'ros2_node = mam_python.ros2_node:main',
+            'ros2_node=mam_python.ros2_node:main',
         ],
     },
+    zip_safe=False,
 )
